1863a6c6ffd2dbe9c3f62df19a6cf1c72ff35082
VJ-Duardo <olafstein501@gmail.com>
2020-03-23 18:41:53 +0100

    added html, css and js

---
+++ js/index.js
@@ -0,0 +1,25 @@
+var thisSite = "https://vj-duardo.github.io/watch-mp4-links/";
+var regex = thisSite + '\\??';
+
+var inputField = document.getElementById("url");
+var video = document.getElementById("video");
+
+inputField.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13){
+        startFromInput();
+    }
+});
+
+
+function startFromUrl() {
+    let url = window.location.href.replace(new RegExp(regex,"g"), '');
+    inputField.value = url;
+    video.src = url;
+}
+
+function startFromInput(){
+    window.location.href = thisSite + "?" + inputField.value;
+}
+
+
+startFromUrl();
